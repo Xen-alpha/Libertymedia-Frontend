adba733c2503d25d9de49d2db1e9658f4e88e35b
Xen-alpha <hky7548@naver.com>
2024-09-27 02:58:39 +0900

    fix: check that min-height CSS attr is useless in body tag with React

--- App.jsx
+++ App.jsx
@@ -7,12 +7,15 @@ import Portal from "./presentation/section_portal";
 export default function App() {
   return (
     <div id="root">
-      <Header />
-      <br />
-      <div id="content">
-        <Portal title="리버티게임" />
-        <Portal title="리버티책" />
-      </div>
+      <header>
+        <Header />
+      </header>
+      <main role="main">
+        <div id="content">
+          <Portal title="리버티게임" />
+          <Portal title="리버티책" />
+        </div>
+      </main>
       <footer>2020, Libertymedia Project</footer>
     </div>
   );
