5bb1ab527c5fde1ccae547782bc85bcd773e62e2
Xen-alpha <hky7548@naver.com>
2024-10-21 19:24:18 +0900

    feat(webpack): add dist directory output

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 
 import path from "path";
 
-export default {
+export default [
+{
   mode: "production",
   entry: "./index.jsx",
   output: {
@@ -50,15 +51,65 @@ export default {
       template: "./index.html",
     }),
   ],
+  resolve: {
+    extensions: [".jsx", ".js", ".css"],
+  },
+  watchOptions: {
+    ignored: ["./node_modules"],
+  },
+},
+{
+  mode: "development",
+  entry: "./index.jsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(
+      import.meta.dirname,
+      "dist"
+    ),
+  },
+  optimization: {
+    minimize: true,
+  },
+  module: {
+    rules: [
+      {
+        // js/jsx를 동시에 같은 방식으로 처리
+        test: /\.(js|jsx)$/, // 인터넷의 예제들을 살펴 보니 보통 여기에는 정규표현식을 쓰는 것 같다.
+        use: "babel-loader", // js가 있는 이유는...공식 도움말의 Shimming을 보자. Polyfill을 써야 하는 이유인 ECMAScript 버전 차이가 주된 이유라고 한다.
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.(png|jpg|bmp)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            publicPath: ".",
+            name: "[name]-[hash].[ext]",
+          },
+        },
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "리버티미디어",
+      template: "./index.html",
+    }),
+  ],
   devServer: {
     static: path.resolve(
       import.meta.dirname,
-      "..",
-      "Libertymedia-Backend",
-      "public"
+      "dist"
     ),
     historyApiFallback: true,
-    port: 8080,
+    port: 3000,
     hot: true,
   },
   resolve: {
@@ -67,4 +118,5 @@ export default {
   watchOptions: {
     ignored: ["./node_modules"],
   },
-};
+}
+];
