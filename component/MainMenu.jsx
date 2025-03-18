76d453dfe7444d2a6e9fbe044e5bd4ed2028624d
Xen-alpha <hky7548@naver.com>
2025-03-18 10:29:04 +0900

    Feat(mainmenu): add box

---
+++ component/MainMenu.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+export default class App extends Component() {
+  render() {
+    return (
+      <div style="float:right; position: relative; right:0;">
+        <div>
+          소개
+        </div>
+        <div>
+          리버티미디어 전개의 장
+        </div>
+        <div>
+          같이 만들어요
+        </div>
+        <div>
+          마이페이지
+        </div>
+        <div>
+          로그아웃
+        </div>
+      </div>
+    );
+  }
+}
