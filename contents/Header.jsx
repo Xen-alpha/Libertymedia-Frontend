bf6e7bf9c69fb073f5c7ca3d08dbd2ef8c31ec76
Xen-alpha <hky7548@naver.com>
2025-03-18 10:23:08 +0900

    Feat(header): add button

--- contents/Header.jsx
+++ contents/Header.jsx
@@ -4,12 +4,20 @@ import "./Header.css";
 
 import LogoImage from "./LogoImage";
 
+const openMenu = () => {
+  console.log("메뉴 버튼 눌림");
+}
+
+
 export default class Header extends Component {
   render() {
     return (
-      <span id="header">
+      <span>
+        <span id="header">
         <LogoImage />
         <span id="header-text">리버티미디어</span>
+        </span>
+        <span id="header-button"><button onClick={openMenu}>〓</button></span>
       </span>
     );
   }
